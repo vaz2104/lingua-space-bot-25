70f88527e3d0ae5f7e846f8fd42bfccc549e26fd
vaz2104 <s.manuliak@gmail.com>
2025-06-09 15:17:42 +0300

    removed logs

--- services/ThirdPartyAPIService.js
+++ services/ThirdPartyAPIService.js
@@ -4,19 +4,19 @@ const fs = require("fs");
 class ThirdPartyAPIService {
   async audioToText(options) {
     const base64 = options?.audio;
-    console.log("base64", base64);
+    // console.log("base64", base64);
 
     const buffer = Buffer.from(base64, "base64");
 
-    console.log("buffer", buffer);
+    // console.log("buffer", buffer);
 
     const fileName = options?.userId || new Date().getTime();
 
-    console.log("fileName", fileName);
+    // console.log("fileName", fileName);
 
     let voice = "./uploads/" + fileName + ".webm";
 
-    console.log("voice", voice);
+    // console.log("voice", voice);
 
     await fs.writeFileSync(voice, buffer);
 
