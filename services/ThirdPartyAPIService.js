b8149edfa01c9b70fd6f7c53ef0d08a773a9fd18
vaz2104 <s.manuliak@gmail.com>
2025-06-09 15:12:25 +0300

    added logs to voice upload funtion

--- services/ThirdPartyAPIService.js
+++ services/ThirdPartyAPIService.js
@@ -4,10 +4,20 @@ const fs = require("fs");
 class ThirdPartyAPIService {
   async audioToText(options) {
     const base64 = options?.audio;
+    console.log("base64", base64);
+
     const buffer = Buffer.from(base64, "base64");
 
+    console.log("buffer", buffer);
+
     const fileName = options?.userId || new Date().getTime();
+
+    console.log("fileName", fileName);
+
     let voice = "./uploads/" + fileName + ".webm";
+
+    console.log("voice", voice);
+
     await fs.writeFileSync(voice, buffer);
 
     const text = await convert(voice);
